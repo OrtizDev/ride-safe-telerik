fba37f1a485dea8d68123313c2fe25b113d11970
Jorge Luis Ortiz Martinez <dev.jorge.om@gmail.com>
2016-12-27 23:24:26 +0000

    asfafasasaffasfaf

---
+++ Ride Safe/dataProviders/baseDatos.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(function() {
+    var provider = app.data.baseDatos = new Everlive({
+        appId: 'it9862gf6meh54pa',
+        scheme: 'https',
+        authentication: {
+            persist: true
+        }
+    });
+
+}());
+
+// START_CUSTOM_CODE_baseDatos
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_baseDatos
