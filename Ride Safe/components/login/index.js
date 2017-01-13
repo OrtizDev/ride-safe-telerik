9b1c24fb86f6babb3d76f9e2918b4ede261f440a
raulzg1 <raul@startbluesoft.com>
2017-01-13 16:32:04 -0600

    added, login function

--- Ride Safe/components/login/index.js
+++ Ride Safe/components/login/index.js
@@ -26,7 +26,46 @@ app.localization.registerView('login');
         /// end global model properties
 
         loginModel = kendo.observable({
-            submit: function() {},
+            submit: function () {
+                var email = $("#email").val();
+                var password = $("#password").val();
+
+                if (email == '') {
+                    alert("Por favor, ingresa tu correo");
+                } else {
+                    var valid = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/;
+                    var validcor = new RegExp(valid);
+                    var matchArray = email.match(validcor);
+                    if (matchArray == null) {
+                        alert("Por favor, ingresa un correo valido");
+                    }
+                    else {
+                        if (password == '') {
+                            alert("Por favor, ingresa tu contraseña");
+                        } else {
+                            var parametros = {
+                                "email": email,
+                                "password": password
+                            };
+
+                            $.ajax({
+                                type: "POST",
+                                url: "http://startbluesoft.com/rideSafeApp/v1/index.php/userlogin",
+                                data: parametros,
+                                dataType: "json",
+                                success: function (data) {
+                                    if (data.error) {
+                                        alert(data.message);
+                                    } else if (!data.error) {
+                                        alert("Logueado");
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+
+            },
             /// start add model functions
             /// end add model functions
 
